3bc2dae1fa8bee8edec5cc2b150b7f630fb63579
atipozitiv <timofejivanov7322@gmail.com>
2024-10-05 02:48:08 +0700

    decorate page, add button and some logic
    add text "no tasks". Button "add" can remove this text. Look beauty

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,25 @@
-// import { hello } from "./sayHI";
+function NoTasks() {
+    if (localStorage.length == 0) {
+        document.body.insertAdjacentHTML('afterend', '<hr class="no-tasks">');
+        document.body.insertAdjacentHTML('afterend', '<div class="no-tasks">No tasks</div>');
+        document.body.insertAdjacentHTML('afterend', '<hr class="no-tasks">');
+    }
+}
+
+window.onload = NoTasks();
+
+function AddTask() {
+    if (localStorage.length == 0) {
+        const elements = document.getElementsByClassName("no-tasks");
+        while(elements.length > 0){
+            elements[0].parentNode.removeChild(elements[0]);
+        }        
+    }
+
+    document.body.appendChild()
+}
+
+let addButtons = document.getElementsByClassName("add-button");
+for (let turn = 0; turn < addButtons.length; ++turn) {
+    addButtons[turn].addEventListener('click', AddTask);
+}
