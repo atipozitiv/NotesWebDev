4ae097df8c6da7c165ecbe1d8ac45cb81f7222fb
atipozitiv <timofejivanov7322@gmail.com>
2024-10-06 21:18:53 +0700

    add delete button logic
    now u can delete tasks

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,5 @@
 function NoTasks() {
-    let noTaskCount = document.getElementsByClassName("no-tasks")
+    let noTaskCount = document.getElementsByClassName("no-tasks");
     if (localStorage.length == 0) {
         if (noTaskCount.length == 0) {
             document.body.insertAdjacentHTML('afterend', '<hr class="no-tasks">');
@@ -14,6 +14,17 @@ function NoTasks() {
     }
 }
 
+function onOpen() {
+    let taskCount = localStorage.length;
+    for (let turn = 0; turn < taskCount; ++turn) {
+        let title = localStorage.key(turn);
+        let about = localStorage.getItem(title);
+        drawTask(title, about);
+    }
+}
+
+let deleteButtons;
+onOpen();
 NoTasks();
 
 let addButton = document.getElementById("add-button");
@@ -27,42 +38,48 @@ addButton.addEventListener("click", function() {
         localStorage.setItem(titleInputValue, aboutInputValue);
         titleInput.value = "";
         aboutInput.value = "";
-
-        let taskCardMainDiv = document.createElement('div');
-        taskCardMainDiv.classList.add('task-block');
-
-        let taskCardTextBlock = document.createElement('div');
-        taskCardTextBlock.classList.add('task-block-text');
-
-        let taskCardTitle = document.createElement('p');
-        taskCardTitle.classList.add('task-block-title');
-        taskCardTitle.innerHTML = titleInputValue;
-
-        let taskCardAbout = document.createElement('p');
-        taskCardAbout.classList.add('task-block-about');
-        taskCardAbout.innerHTML = aboutInputValue;
-
-        let taskCardButton = document.createElement('button');
-        taskCardButton.classList.add('task-block-button');
-        taskCardButton.innerHTML = "×";
-
-        taskCardTextBlock.appendChild(taskCardTitle);
-        taskCardTextBlock.appendChild(taskCardAbout);
-        taskCardMainDiv.appendChild(taskCardTextBlock);
-        taskCardMainDiv.appendChild(taskCardButton);
-
-        document.body.appendChild(taskCardMainDiv);
+        drawTask(titleInputValue, aboutInputValue);
     }
-
     NoTasks();
-
-    
-    // drawTasks();
 }); 
 
-function drawTasks() {
-    let taskCount = localStorage.length;
-    for (let turn = 0; turn < taskCount; ++turn) {
-        
+function drawTask(title, about) {
+    let taskCardMainDiv = document.createElement('div');
+    taskCardMainDiv.classList.add('task-block');
+
+    let taskCardTextBlock = document.createElement('div');
+    taskCardTextBlock.classList.add('task-block-text');
+
+    let taskCardTitle = document.createElement('p');
+    taskCardTitle.classList.add('task-block-title');
+    taskCardTitle.innerHTML = title;
+
+    let taskCardAbout = document.createElement('p');
+    taskCardAbout.classList.add('task-block-about');
+    taskCardAbout.innerHTML = about;
+
+    let taskCardButton = document.createElement('button');
+    taskCardButton.classList.add('task-block-button');
+    taskCardButton.innerHTML = "×";
+
+    taskCardTextBlock.appendChild(taskCardTitle);
+    taskCardTextBlock.appendChild(taskCardAbout);
+    taskCardMainDiv.appendChild(taskCardTextBlock);
+    taskCardMainDiv.appendChild(taskCardButton);
+
+    document.body.appendChild(taskCardMainDiv);
+    GetDeleteButtons();
+}
+
+GetDeleteButtons();
+function GetDeleteButtons() {
+    deleteButtons = document.getElementsByClassName("task-block-button");
+    for (let turn = 0; turn < deleteButtons.length; ++turn) {
+        deleteButtons[turn].addEventListener('click', function() {
+            let storageKey = this.parentNode.getElementsByClassName("task-block-title")[0].innerHTML;
+            localStorage.removeItem(storageKey);
+            NoTasks();
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        })
     }
 }
