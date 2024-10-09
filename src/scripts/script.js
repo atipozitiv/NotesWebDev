b2aae4209e72239c5b45c4eefcc16138263fca84
atipozitiv <117564743+atipozitiv@users.noreply.github.com>
2024-10-09 11:04:20 +0700

    fix bugs, clean code
    fix bug with special buttons apearing

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,5 @@
-function NoTasks() {
-    let noTaskCount = document.getElementsByClassName("no-tasks");
+function noTasks() {
+    const noTaskCount = document.getElementsByClassName("no-tasks");
     if (localStorage.length == 0) {
         if (noTaskCount.length == 0) {
             document.body.insertAdjacentHTML('afterend', '<hr class="no-tasks">');
@@ -15,26 +15,26 @@ function NoTasks() {
 }
 
 function onOpen() {
-    let taskCount = localStorage.length;
+    const taskCount = localStorage.length;
     for (let turn = 0; turn < taskCount; ++turn) {
         let title = localStorage.key(turn);
         let about = localStorage.getItem(title);
-        drawTask(title, about);
+        drawTasks(title, about);
     }
 }
 
-function drawTask(title, about) {
-    let taskCardMainDiv = document.createElement('div');
+function drawTasks(title, about) {
+    const taskCardMainDiv = document.createElement('div');
     taskCardMainDiv.classList.add('task-block');
-    let taskCardTextBlock = document.createElement('div');
+    const taskCardTextBlock = document.createElement('div');
     taskCardTextBlock.classList.add('task-block-text');
-    let taskCardTitle = document.createElement('p');
+    const taskCardTitle = document.createElement('p');
     taskCardTitle.classList.add('task-block-title');
     taskCardTitle.innerHTML = title;
-    let taskCardAbout = document.createElement('p');
+    const taskCardAbout = document.createElement('p');
     taskCardAbout.classList.add('task-block-about');
     taskCardAbout.innerHTML = about;
-    let taskCardButton = document.createElement('button');
+    const taskCardButton = document.createElement('button');
     taskCardButton.classList.add('task-block-button');
     taskCardButton.innerHTML = "×";
 
@@ -45,107 +45,99 @@ function drawTask(title, about) {
     document.body.appendChild(taskCardMainDiv);
 }
 
-function DeleteSpecButtons() {
+function deleteSpecButtons() {
     const elements = document.getElementsByClassName("spec-buttons");
     while(elements.length > 0) {
         elements[0].parentNode.removeChild(elements[0]);
     }   
 }
 
-function ClosePopup() {
+function closePopup() {
     const elements = document.getElementsByClassName("popup-background");
     while(elements.length > 0){
         elements[0].parentNode.removeChild(elements[0]);
     } 
 }
 
-onOpen();
-NoTasks();
-let activeTask;
-let titleToCopy = "";
-let aboutToCopy = "";
+function createSpecButtons() {
+    let specButtons = document.createElement('div');
+    specButtons.classList.add('spec-buttons');
+    let shareButton = document.createElement('button');
+    shareButton.classList.add('share-button');
+    let infoButton = document.createElement('button');
+    infoButton.classList.add('info-button');
+    let editButton = document.createElement('button');
+    editButton.classList.add('edit-button');
+    specButtons.appendChild(shareButton);
+    specButtons.appendChild(infoButton);
+    specButtons.appendChild(editButton);
+    return specButtons;
+}
 
-let addButton = document.getElementById("add-button");
+onOpen();
+noTasks();
+let activeTask;
+
+const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", function() {
-    let titleInput = document.getElementById("title-input");
+    const titleInput = document.getElementById("title-input");
     let titleInputValue = titleInput.value;
-    let aboutInput = document.getElementById("about-input");
+    const aboutInput = document.getElementById("about-input");
     let aboutInputValue = aboutInput.value;
 
     if(!((titleInputValue.length == 0) && (aboutInputValue.length == 0))) {
         localStorage.setItem(titleInputValue, aboutInputValue);
         titleInput.value = "";
         aboutInput.value = "";
-        drawTask(titleInputValue, aboutInputValue);
+        drawTasks(titleInputValue, aboutInputValue);
     }
-    NoTasks();
+    noTasks();
 }); 
 
 document.body.addEventListener('click', function(event) {
+    if (event.target.classList == "task-block-text") {
+        if (activeTask == event.target) {
+            deleteSpecButtons();
+            activeTask = null;
+        } else {
+            activeTask = event.target;
+            deleteSpecButtons();
+            event.target.parentNode.insertAdjacentElement('afterend', createSpecButtons());
+        }
+    }
     if (event.target.classList == "task-block") {
         if (activeTask == event.target.getElementsByClassName("task-block-text")[0]) {
-            DeleteSpecButtons();
+            deleteSpecButtons();
             activeTask = null;
         } else {
             activeTask = event.target.getElementsByClassName("task-block-text")[0];
-            titleToCopy = event.target.getElementsByClassName("task-block-text")[0].getElementsByClassName("task-block-title")[0].textContent;
-            aboutToCopy = event.target.getElementsByClassName("task-block-text")[0].getElementsByClassName("task-block-about")[0].textContent;
-            textToCopy = titleToCopy + " \n\n" + aboutToCopy;
-            DeleteSpecButtons();
-
-            let specButtons = document.createElement('div');
-            specButtons.classList.add('spec-buttons');
-            let shareButton = document.createElement('button');
-            shareButton.classList.add('share-button');
-            let infoButton = document.createElement('button');
-            infoButton.classList.add('info-button');
-            let editButton = document.createElement('button');
-            editButton.classList.add('edit-button');
-            specButtons.appendChild(shareButton);
-            specButtons.appendChild(infoButton);
-            specButtons.appendChild(editButton);
-            event.target.insertAdjacentElement('afterend', specButtons);
+            deleteSpecButtons();
+            event.target.insertAdjacentElement('afterend', createSpecButtons());
         }
     }
-
     if ((event.target.classList == "task-block-title") || (event.target.classList == "task-block-about")) {
         if (activeTask == event.target.parentNode) {
-            DeleteSpecButtons();
+            deleteSpecButtons();
             activeTask = null;
         } else {
             activeTask = event.target.parentNode;
-            titleToCopy = activeTask.getElementsByClassName("task-block-title")[0].textContent;
-            aboutToCopy = activeTask.getElementsByClassName("task-block-about")[0].textContent;
-            textToCopy = titleToCopy + " \n\n" + aboutToCopy;
-            DeleteSpecButtons();
-
-            let specButtons = document.createElement('div');
-            specButtons.classList.add('spec-buttons');
-            let shareButton = document.createElement('button');
-            shareButton.classList.add('share-button');
-            let infoButton = document.createElement('button');
-            infoButton.classList.add('info-button');
-            let editButton = document.createElement('button');
-            editButton.classList.add('edit-button');
-            specButtons.appendChild(shareButton);
-            specButtons.appendChild(infoButton);
-            specButtons.appendChild(editButton);
-            event.target.parentNode.parentNode.insertAdjacentElement('afterend', specButtons);
+            deleteSpecButtons();
+            event.target.parentNode.parentNode.insertAdjacentElement('afterend', createSpecButtons());
         }
     }
 
     if (event.target.classList == "task-block-button") {
-        let backgroundDiv = document.createElement('div');
+        const backgroundDiv = document.createElement('div');
         backgroundDiv.classList.add('popup-background');
-        let mainDiv = document.createElement('div');
+        const mainDiv = document.createElement('div');
         mainDiv.classList.add('delete-popup');
-        let deleteText = document.createElement('p');
+        const deleteText = document.createElement('p');
         deleteText.innerHTML = "Delete this task?";
-        let buttonsDiv = document.createElement('div');
-        let buttonYes = document.createElement('button');
+        const buttonsDiv = document.createElement('div');
+        const buttonYes = document.createElement('button');
         buttonYes.classList.add('delete-popup-button-yes');
         buttonYes.innerHTML = "Yes";
-        let buttonNo = document.createElement('button');
+        const buttonNo = document.createElement('button');
         buttonNo.classList.add('delete-popup-button-no');
         buttonNo.innerHTML = "No";
         buttonsDiv.appendChild(buttonYes);
@@ -158,30 +150,30 @@ document.body.addEventListener('click', function(event) {
         buttonYes.addEventListener('click', function() {
             let storageKey = event.target.parentNode.getElementsByClassName("task-block-title")[0].innerHTML;
             localStorage.removeItem(storageKey);
-            NoTasks();
-            DeleteSpecButtons();
+            noTasks();
+            deleteSpecButtons();
             event.target.parentNode.parentNode.removeChild(event.target.parentNode);
-            ClosePopup(); 
+            closePopup(); 
         })
         buttonNo.addEventListener('click', function() {
-            ClosePopup(); 
+            closePopup(); 
         })
     }
 
     if (event.target.classList == "share-button") {
-        let backgroundDiv = document.createElement('div');
+        const backgroundDiv = document.createElement('div');
         backgroundDiv.classList.add('popup-background');
-        let mainDiv = document.createElement('div');
+        const mainDiv = document.createElement('div');
         mainDiv.classList.add('share-popup');
-        let toCopyButton = document.createElement('button');
+        const toCopyButton = document.createElement('button');
         toCopyButton.classList.add('to-copy-button');
-        let vkButton = document.createElement('button');
+        const vkButton = document.createElement('button');
         vkButton.classList.add('vk-button');
-        let telegramButton = document.createElement('button');
+        const telegramButton = document.createElement('button');
         telegramButton.classList.add('telegram-button');
-        let watsappButton = document.createElement('button');
+        const watsappButton = document.createElement('button');
         watsappButton.classList.add('watsapp-button')
-        let facebookButton = document.createElement('button');
+        const facebookButton = document.createElement('button');
         facebookButton.classList.add('facebook-button');
         mainDiv.appendChild(toCopyButton);
         mainDiv.appendChild(vkButton);
@@ -192,24 +184,24 @@ document.body.addEventListener('click', function(event) {
         document.body.appendChild(backgroundDiv);
 
         toCopyButton.addEventListener('click', function() {
-            navigator.clipboard.writeText(textToCopy);
-            ClosePopup();
+            navigator.clipboard.writeText(activeTask.getElementsByClassName("task-block-title")[0].textContent + " \n\n" + activeTask.getElementsByClassName("task-block-about")[0].textContent);
+            closePopup();
         })
         vkButton.addEventListener('click', function() {
-            navigator.clipboard.writeText(textToCopy);
-            ClosePopup();
+            navigator.clipboard.writeText(activeTask.getElementsByClassName("task-block-title")[0].textContent + " \n\n" + activeTask.getElementsByClassName("task-block-about")[0].textContent);
+            closePopup();
         })
         telegramButton.addEventListener('click', function() {
-            navigator.clipboard.writeText(textToCopy);
-            ClosePopup();
+            navigator.clipboard.writeText(activeTask.getElementsByClassName("task-block-title")[0].textContent + " \n\n" + activeTask.getElementsByClassName("task-block-about")[0].textContent);
+            closePopup();
         })
         watsappButton.addEventListener('click', function() {
-            navigator.clipboard.writeText(textToCopy);
-            ClosePopup();
+            navigator.clipboard.writeText(activeTask.getElementsByClassName("task-block-title")[0].textContent + " \n\n" + activeTask.getElementsByClassName("task-block-about")[0].textContent);
+            closePopup();
         })
         facebookButton.addEventListener('click', function() {
-            navigator.clipboard.writeText(textToCopy);
-            ClosePopup();
+            navigator.clipboard.writeText(activeTask.getElementsByClassName("task-block-title")[0].textContent + " \n\n" + activeTask.getElementsByClassName("task-block-about")[0].textContent);
+            closePopup();
         })
     }
 
@@ -218,21 +210,21 @@ document.body.addEventListener('click', function(event) {
     }
 
     if (event.target.classList == "edit-button") {
-        let backgroundDiv = document.createElement('div');
+        const backgroundDiv = document.createElement('div');
         backgroundDiv.classList.add('popup-background');
-        let mainDiv = document.createElement('div');
+        const mainDiv = document.createElement('div');
         mainDiv.classList.add('edit-popup');
-        let titleEditInput = document.createElement('input');
+        const titleEditInput = document.createElement('input');
         titleEditInput.classList.add('title-edit');
-        titleEditInput.value = titleToCopy;
-        let aboutEditArea = document.createElement('textarea');
+        titleEditInput.value = activeTask.getElementsByClassName("task-block-title")[0].textContent;
+        const aboutEditArea = document.createElement('textarea');
         aboutEditArea.classList.add('about-edit');
-        aboutEditArea.value = aboutToCopy;
-        let buttonsDiv = document.createElement('div');
+        aboutEditArea.value = activeTask.getElementsByClassName("task-block-about")[0].textContent;
+        const buttonsDiv = document.createElement('div');
         buttonsDiv.classList.add('popup-edit-buttons');
-        let buttonCancel = document.createElement('button');
+        const buttonCancel = document.createElement('button');
         buttonCancel.innerHTML = "Cancel";
-        let buttonSave = document.createElement('button');
+        const buttonSave = document.createElement('button');
         buttonSave.innerHTML = "Save";
         buttonsDiv.appendChild(buttonCancel);
         buttonsDiv.appendChild(buttonSave);
@@ -243,25 +235,25 @@ document.body.addEventListener('click', function(event) {
         document.body.appendChild(backgroundDiv);        
 
         buttonSave.addEventListener('click', function() {
-            let titleChangedText = titleEditInput.value;
-            let aboutChangedText = aboutEditArea.value;
+            const titleChangedText = titleEditInput.value;
+            const aboutChangedText = aboutEditArea.value;
 
             activeTask.parentNode.parentNode.removeChild(activeTask.parentNode);
-            DeleteSpecButtons();
+            deleteSpecButtons();
             let storageKey = activeTask.parentNode.getElementsByClassName("task-block-title")[0].innerHTML;
             localStorage.removeItem(storageKey);
             localStorage.setItem(titleChangedText, aboutChangedText);
-            drawTask(titleChangedText, aboutChangedText);
-            ClosePopup(); 
+            drawTasks(titleChangedText, aboutChangedText);
+            closePopup(); 
         })
 
         buttonCancel.addEventListener('click', function() {
-            ClosePopup();
-            DeleteSpecButtons();
+            closePopup();
+            deleteSpecButtons();
         })
     }
 
     if (event.target.classList == "popup-background") {
-        ClosePopup();
+        closePopup();
     }
 })
